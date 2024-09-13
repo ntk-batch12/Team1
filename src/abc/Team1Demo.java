4d3129e25b5682e793d433d7d6acda573d18d7c7
Afghanzada <Hasibafghanzada10@gmail.com>
2024-09-12 18:08:49 -0700

    Update Team1Demo.java

--- src/abc/Team1Demo.java
+++ src/abc/Team1Demo.java
@@ -8,6 +8,9 @@ public class Team1Demo {
 	public static void Add1() {
 		System.out.println("code one");
 	}
+	public static void add2() {
+		System.out.println("code two"):
+			}
 
 	//this is Hasib from Github
 	//create (add2 method)
