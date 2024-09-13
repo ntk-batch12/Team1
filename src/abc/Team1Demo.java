87507c932559d873ef3c2109d3cab53ad54ebe93
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-12 21:09:28 -0400

    Update Team1Demo.java

--- src/abc/Team1Demo.java
+++ src/abc/Team1Demo.java
@@ -1,13 +1,7 @@
 package abc;
 
 public class Team1Demo {
-	public static void main(String[] args) {
-		System.out.println("Team 1...");
-	}
-	
-	public static void Add1() {
-		System.out.println("code one");
-	}
+	 
 	public static void add2() {
 		System.out.println("code two"):
 			}
