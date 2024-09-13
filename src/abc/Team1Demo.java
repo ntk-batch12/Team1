43ed44ace5821f745fd14686aaf6932ecdf5a6d4
Sajad Ahmadzai <Sajad Ahmadzai@10.0.0.155>
2024-09-12 21:03:46 -0400

    ready for push

--- src/abc/Team1Demo.java
+++ src/abc/Team1Demo.java
@@ -4,6 +4,10 @@ public class Team1Demo {
 	public static void main(String[] args) {
 		System.out.println("Team 1...");
 	}
+	
+	public static void Add1() {
+		System.out.println("code one");
+	}
 
 	//this is Hasib from Github
 	//create (add2 method)
